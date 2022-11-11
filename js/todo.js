8f4c0167f2f0c5e5a0607ff5a2cf1b0290aab45c
yejin <kwonyj1022@naver.com>
2022-11-11 18:36:59 +0900

    feat : To Do - loading To Do

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
 const todos = [];
 
 function saveTodos() {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 function onDeleteTodoClick(event) {
@@ -35,3 +37,9 @@ function onTodoSubmit(event) {
 }
 
 todoForm.addEventListener("submit", onTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos) {
+    const parsedTodos = JSON.parse(savedTodos);
+    parsedTodos.forEach(printTodo);
+}
